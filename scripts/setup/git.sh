1a0137080b035f0a60e252ced1b7b646e6c9a5df
omeyenburg <omeyenburg@gmail.com>
2026-01-12 09:32:44 +0100

    feat: enable gpg signing for git

--- scripts/setup/git.sh
+++ scripts/setup/git.sh
@@ -9,4 +9,10 @@ git config --global fetch.autoFetch true
 git config --global color.ui true
 git config --global init.defaultBranch main
 
-echo "Git configuration applied."
+# Enable commit signing with GPG
+git config --global user.signingkey 455ABBC1D8C33DA9
+git config --global commit.gpgsign true
+git config --global gpg.format openpgp
+git config --global gpg.program gpg
+
+echo "Git configuration applied, GPG signing enabled."
